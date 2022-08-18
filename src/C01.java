7e4669a620d88e9355638d35941893072fe893fa
Mehmet Bedrettin Aktas <mehmetbedrettinaktas@gmail.com>
2022-08-18 23:24:41 +0200

    V2

--- src/C01.java
+++ src/C01.java
@@ -2,5 +2,6 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("1.Deneme ");
         System.out.println("1.Deneme Github");
+        System.out.println("1.Deneme mehmet");
     }
 }
